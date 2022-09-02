ace6efdb55ce25c2918f7ac0ef89679bb49115de
Grasiela Gomes <produtoradama@gmail.com>
2022-09-01 21:21:32 -0300

    feat: add basic template

---
+++ layouts/BasicTemplate.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Box } from "@chakra-ui/react";
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+export const BasicTemplate: FC<Props> = ({ children }) => {
+  return (
+    <Box maxW="1200px" mx="auto" p="10">
+      {children}
+    </Box>
+  );
+};
