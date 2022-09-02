21ad5711af931db91667e50a548e7267d1bf024b
Grasiela Gomes <produtoradama@gmail.com>
2022-09-01 21:21:08 -0300

    feat: add document

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from "next/document";
+import theme from "../themes";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initalProps = await Document.getInitialProps(ctx);
+
+    return initalProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
