64710a7ffe61fb37f7dbfea21a04b950558b0c3a
Grasiela Gomes <produtoradama@gmail.com>
2022-09-02 19:18:35 -0300

    feat: add custom tokens to theme

--- themes/index.ts
+++ themes/index.ts
@@ -5,11 +5,32 @@ const theme = extendTheme({
   styles: {
     global: (props: { colorMode: string }) => ({
       body: {
-        backgroundColor: props.colorMode === "dark" ? "gray.900" : "gray.200"
+        backgroundColor: props.colorMode === "dark" ? "dark.900" : "white"
       }
     })
   },
-  colors: {}
+  colors: {
+    brand: {
+      blue: "#3694FF",
+      green: "#0F9549",
+      pink: "#FF0080",
+      purple: "#7928CA",
+      yellow: "#ECC94B"
+    },
+    light: {
+      50: "#F9FAFB",
+      100: "#F3F4F6",
+      200: "#E5E7EB"
+    },
+    dark: {
+      700: "#374151",
+      800: "#1F2937",
+      900: "#111827"
+    }
+  },
+  shadows: {
+    card: "1px 6px 20px #02384a1b"
+  }
 });
 
 export default theme;
