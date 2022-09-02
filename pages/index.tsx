ea46474decffd3adbde0f1736a383100dc39b466
Grasiela Gomes <produtoradama@gmail.com>
2022-09-02 19:17:56 -0300

    refactor: change HomePage UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { Stack } from "@chakra-ui/react";
+import { BasicTemplate } from "../components/layouts";
+import { PageHeader } from "../components/pageHeader";
+import { TodoContainer } from "../components/todo";
 
 const HomePage: NextPage = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <BasicTemplate>
+      <Stack spacing="6">
+        <PageHeader numberTasks="2" title="My Board" />
+        <TodoContainer />
+      </Stack>
+    </BasicTemplate>
+  );
 };
 
 export default HomePage;
