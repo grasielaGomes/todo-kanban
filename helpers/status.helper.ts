e710450ab5858ff32280be90a20eb82974cbb2de
Grasiela Gomes <produtoradama@gmail.com>
2022-09-04 16:23:25 -0300

    feat: add new helper files

---
+++ helpers/status.helper.ts
@@ -0,0 +1,7 @@
+export const STATUS_COLUMNS: string[] = ["todo", "progress", "done"];
+
+export const STATUS_TITLES: { [key: string]: string } = {
+  todo: "Todo",
+  progress: "In Progress",
+  done: "Done"
+};
---
+++ helpers/taskTypes.helper.ts
@@ -0,0 +1,7 @@
+export const TYPES: string[] = ["discovery", "feature", "bug"];
+
+export const TYPES_TITLES: { [key: string]: string } = {
+  discovery: "Discovery",
+  feature: "New Feature",
+  bug: "Bug Fix"
+};
