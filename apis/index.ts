fbffeab8cb9d8154812d14dd5301f9525b7a4471
Grasiela Gomes <produtoradama@gmail.com>
2022-09-06 13:43:03 -0300

    feat: export new files

--- apis/index.ts
+++ apis/index.ts
@@ -1,1 +1,5 @@
 export { default as tasksApi } from "./tasksApi";
+export * from "./addTaskApi";
+export * from "./deleteTaskApi";
+export * from "./getTasksApi";
+export * from "./updateTaskApi";
--- helpers/index.ts
+++ helpers/index.ts
@@ -1,2 +1,3 @@
 export * from "./status.helper";
 export * from "./taskTypes.helper";
+export * from "./environment";
