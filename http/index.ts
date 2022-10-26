7a4f4c29ae747e3a8020ab6b543a1610308c0891
Grasiela Gomes <produtoradama@gmail.com>
2022-10-26 15:20:18 -0300

    feat: add new exports

---
+++ http/index.ts
@@ -0,0 +1,6 @@
+export * from "./AxiosAdapter";
+export * from "./HttpDeleteClient";
+export * from "./HttpGetClient";
+export * from "./HttpPostClient";
+export * from "./HttpPutClient";
+export * from "./HttpResponse";
