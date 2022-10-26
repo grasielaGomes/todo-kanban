71c73c696fd67ffa992696a1d4aba2968d0720b8
Grasiela Gomes <produtoradama@gmail.com>
2022-10-26 15:21:19 -0300

    feat: add head tag

--- components/layouts/BasicTemplate.tsx
+++ components/layouts/BasicTemplate.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import { BasicTemplateI } from "./";
 
@@ -8,7 +9,9 @@ export const BasicTemplate: FC<BasicTemplateI> = ({
 }) => {
   return (
     <Box maxW="1200px" mx="auto" p={["5", "10"]}>
-      <title>{title}</title>
+      <Head>
+        <title>{title}</title>
+      </Head>
       {children}
     </Box>
   );
